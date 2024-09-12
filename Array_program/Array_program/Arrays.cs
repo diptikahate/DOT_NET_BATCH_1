aa1087fab0951f790d2a7616f3ff23b8c88f3114
diptikahate <111509464+diptikahate@users.noreply.github.com>
2024-09-12 12:30:23 +0530

    Basic Array Program

---
+++ Array_program/Array_program/Arrays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_program
+{
+    internal class Arrays
+    {
+        int[] items;
+        int index;
+        public const int MAX_ITEM = 10;
+
+
+        public Arrays()
+        {
+             items = new int[MAX_ITEM];
+            index = 0;
+        }
+
+        public void Add(int data)
+        {
+            if (index == items.Length)
+            {
+                return;
+            }
+
+            items[index++] = data;
+        }
+
+        public bool Remove(int data)
+        {
+                int itemIndex = Indexof(data);
+            if (itemIndex == -1)
+            {
+                return false;
+            }
+            else
+            {
+                items[itemIndex] = items[MAX_ITEM - 1];
+                index--;
+                return true;
+            }
+
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+
+        }
+
+        public int Indexof(int data)
+        {
+            for (int i = 0; i < MAX_ITEM; i++)
+            {
+                if (items[i] == data)
+                {
+                    return i;
+                }  
+            }
+            return -1;
+
+        }
+    }
+}
---
+++ Array_program/Array_program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arrays refArray = new Arrays();
+            for (int i = 0; i < Arrays.MAX_ITEM; i++){
+                refArray.Add(i);
+                refArray.Print();
+                refArray.Remove(5);
+                refArray.Print();
+                Console.ReadLine();
+            }
+        }
+    }
+}
