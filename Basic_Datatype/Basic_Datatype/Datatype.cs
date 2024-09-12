6b574cf1fdee92292d4af37d510730d7f64ab286
diptikahate <111509464+diptikahate@users.noreply.github.com>
2024-09-12 12:22:25 +0530

    Basic Datatype

---
+++ Basic_Datatype/Basic_Datatype/Datatype.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Datatype
+{
+
+    class MyInt
+    {
+        public int Value;
+
+        public MyInt(int value)
+        {
+            Value = value;
+            Console.WriteLine("Initial MyInt value:{0}", Value);
+        }
+
+        public void Addition(int number)
+        {
+            Value += number;
+            Console.WriteLine("After adding 5:{0} ", Value);
+        }
+
+        public void Subtraction(int number)
+        {
+            Value -= number;
+            Console.WriteLine("After subtracting 3:{0} ", Value);
+        }
+
+    }
+
+    class MyDouble
+    {
+        public double Value;
+
+        public MyDouble(double value)
+        {
+            Value = value;
+            Console.WriteLine("\nInitial MyDouble value:{0} ", Value);
+        }
+
+        public void Multiplication(double number)
+        {
+            Value *= number;
+            Console.WriteLine("After multiplying by 2: {0} ", Value);
+        }
+
+        public void Divide(double number)
+        {
+            if (number != 0)
+            {
+                Value /= number;
+                Console.WriteLine("After dividing by 3: {0} ", Value);
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+        }
+    }
+}
---
+++ Basic_Datatype/Basic_Datatype/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Datatype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create MyInt object
+            MyInt myInt = new MyInt(10);
+            myInt.Addition(5);
+            myInt.Subtraction(3);
+          
+            // Create MyDouble object
+            MyDouble myDouble = new MyDouble(4.5);
+            myDouble.Multiplication(2);
+            myDouble.Divide(3);
+            Console.ReadLine();
+        }
+    }
+}
