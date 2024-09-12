3c2d6749d59b6f9589eb90b107c56961f4c55a69
diptikahate <111509464+diptikahate@users.noreply.github.com>
2024-09-12 12:26:35 +0530

    Unorder Dynamic Array

---
+++ Unorder_dynamic_Array/Unorder_dynamic_Array/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DynamicArrays.UnorderedIntDynamicArray unDArr = new DynamicArrays.UnorderedIntDynamicArray();
+            const int numberOfDataEntry = 10;
+
+            for (int i = 0; i < numberOfDataEntry; i++)
+            {
+                unDArr.Add((i + 1) * 2);
+            }
+            Console.WriteLine("after Adding element in aaray");
+            Console.WriteLine(unDArr.ToString());
+            unDArr.Remove(2);
+            Console.WriteLine("After Removing 2 from array");
+            Console.WriteLine(unDArr.ToString());
+
+            Console.WriteLine("Adding More element in array");
+            for (int i = 0; i < numberOfDataEntry; i++)
+            {
+                unDArr.Add((i + 2) * 2);
+            }
+            Console.WriteLine(unDArr.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
+
