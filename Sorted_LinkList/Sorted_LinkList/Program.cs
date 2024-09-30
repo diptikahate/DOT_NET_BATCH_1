87baa08f756de400a7251be6f8f89f58d2f764f6
diptikahate <111509464+diptikahate@users.noreply.github.com>
2024-09-30 13:07:46 +0530

    Add files via upload
    Sorted Link List

---
+++ Sorted_LinkList/Sorted_LinkList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted_LinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sorted_LinkList sll = new Sorted_LinkList();
+            sll.Insert(2);
+            sll.displayMethod();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Sorted_LinkList/Sorted_LinkList/Sorted_LinkList.cs
@@ -0,0 +1,62 @@
+﻿using CSharp_Exam;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted_LinkList
+{
+    internal class Sorted_LinkList
+    {
+        protected Node headNode;
+        public Sorted_LinkList()
+        {
+            headNode = new Node();
+            headNode.prev = headNode;
+            headNode.next = headNode;
+        }
+        public void displayMethod()
+        {
+            Console.Write(" LinkList : ");
+            Node tempNode = headNode.next;
+            while (tempNode != headNode)
+            {
+                Console.Write("[" + tempNode.data + "]->");
+                tempNode = tempNode.next;
+            }
+            Console.WriteLine("[END]");
+        }
+
+        public void Insert(int data)
+        {
+            Node tempNode = headNode.next;
+            while (tempNode != headNode)
+            {
+               
+                    Node newNode = new Node(data);
+                if (tempNode.data < data)
+                {
+                    GenericInsert(tempNode.prev, newNode, tempNode);
+                }
+                else
+                {
+                    GenericInsert(tempNode, newNode, tempNode.next);
+                }
+                    
+                
+            }
+
+        }
+        protected static void GenericInsert(Node start, Node mid, Node end)
+        {
+            start.next = mid;
+            mid.next = end;
+            end.prev = mid;
+            mid.prev = start;
+        }
+
+      
+
+    }
+}
